8086b46805e50e1ed4e314817183e03d65517c0a
ElenaPT <elenapt95@gmail.com>
2021-11-04 12:41:07 +0100

    Send a single payment via a test Interledger Network (when the page is visited)

--- src/components/Extension.js
+++ src/components/Extension.js
@@ -5,6 +5,8 @@ import Paper from '@mui/material/Paper'
 import Container from '@mui/material/Container'
 import {withStyles} from '@mui/styles'
 
+var http = require('http');
+
 
 const styles = {
   paper: {
@@ -22,7 +24,8 @@ class Extension extends Component {
     super(props)
 
     this.state = {
-      paymentPointer: "None"
+      paymentPointer: "None",
+      flag: 0
     }
   }
 
@@ -30,23 +33,62 @@ class Extension extends Component {
         return document.querySelector("meta[name='monetization']").getAttribute("content")
   }*/
 
-    getPaymentPointer() {
-      let s = (v) => this.setState({paymentPointer: v});
+  /**Gets payment pointer from the header of the web and saves the value in the state**/
+  getPaymentPointer() {
+    let s = (v) => this.setState({paymentPointer: v});
 
-      console.log('(' + this.modifyDOM + ')();')
+    new Promise(function(resolve, reject) {
+      chrome.tabs.executeScript({
+          code: '(function(){return document.querySelector("meta[name=\'monetization\']").getAttribute("content")})()'
+      }, (results) => {
+          resolve(results[0])
+      });
+    }).then(function(value){
+      s(value)
+    })
+  }
 
-      new Promise(function(resolve, reject) {
-        chrome.tabs.executeScript({
-            code: '(function(){return document.querySelector("meta[name=\'monetization\']").getAttribute("content")})()'
-        }, (results) => {
-            resolve(results[0])
-            console.log(results[0])
-        });
-      }).then(function(value){
-        s(value)
-        console.log("Promesa", value)
-      })
+
+  /**Sends a singular payment via the Interledger Network**/
+  sendIlpPayment = () => {
+    console.log("sendIlpPayment begins")
+
+    const data = JSON.stringify({
+      "receiver": "http://charlie-node:7770/accounts/charlie/spsp",
+      "source_amount": 20
+    })
+
+    const options = {
+      hostname: '192.168.128.4',
+      port: 7770,
+      path: '/accounts/' + 'alice' + '/payments',
+      method: 'POST',
+      headers: {
+        'Authorization': 'Bearer alice_password',
+        'accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Content-Length': data.length,
+      }
     }
+
+    const req = http.request(options, res2 => {
+
+      res2.on('data', d => {
+        console.log('200',d);
+      })
+    })
+
+    req.on('error', error => {
+      console.log('500', 'Internal Server Error')
+      console.error(error)
+    })
+
+    req.write(data)
+    req.end()
+
+    var txt = "Amount: " + 20 + ", from: " + "alice" + ", to: " + "http://charlie-node:7770/accounts/charlie/spsp";
+    console.log(txt);
+  }
 
 
   render(){
@@ -54,6 +96,11 @@ class Extension extends Component {
     const {classes} = this.props
 
     this.getPaymentPointer();
+
+    if(this.state.flag==0){
+      this.sendIlpPayment();
+      this.setState({flag:1})
+    }
 
     return(
       <Paper
