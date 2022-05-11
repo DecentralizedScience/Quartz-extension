20cb1ce20d22ae1fcf3e75cb0b6e97c2167e4b61
ElenaPT <elenapt95@gmail.com>
2022-05-11 18:58:06 +0200

    First draft ui for the register and login

---
+++ Register/React ui/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import SignUp from './SignUp.js'
+import SignUpTest from './SignUpTest.js'
+
+function App() {
+  return (
+        <SignUp/>
+  );
+}
+
+export default App;
